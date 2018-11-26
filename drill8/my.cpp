d5e837a7da726b4d0f5d6831d30d944ddee04e71
Ignissen <38163224+Ignissen@users.noreply.github.com>
2018-11-26 13:11:22 +0100

    Add files via upload

---
+++ drill8/my.cpp
@@ -0,0 +1,17 @@
+#include "my.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+void print_foo()
+{
+    cout<<foo;
+}
+
+void print(int x)
+{
+    cout<<x;
+}
---
+++ drill8/my.h
@@ -0,0 +1,9 @@
+#ifndef MY_H
+#define MY_H
+
+
+extern int foo;
+void print_foo();
+void print(int);
+
+#endif // MY_H
